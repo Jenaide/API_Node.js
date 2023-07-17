5eb27337e5dc878d3b579d3daa85110a965f0f5d
Jenaide Sibolie <jenaidesibolie@gmail.com>
2023-07-17 23:04:54 +0200

    added route handlers

--- server.js
+++ server.js
@@ -1,10 +1,85 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/productModels');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 const server = app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('The server is listening on http://localhost:${PORT}'));
+// Route handler for getting all products from database
+app.get('/products', async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
+// Route handler for getting a single product from database
+app.get('/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+// Route handler to update a single product from database
+app.put('/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).json({message: `Product not found with id ${id}`})
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+    
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+// Route handler to delete product from the database
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).json({message: `Product not found with id ${id}`})
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+// Route handler for posting product to database
+app.post('/product', async(req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
+// Connect to the database
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/node_api')
+  .then(() => {
+    console.log('Connected to database')
+    server.listen(PORT, () => console.log('The server is listening on http://localhost:${PORT}'));
+  }).catch((err) => {
+    console.log('Error connecting to database')
+  });
